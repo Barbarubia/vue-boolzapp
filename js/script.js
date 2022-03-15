d58d9ea7e56322e912773b3cbc2c7de93cfdbf73
Barbarubia <dnl.desimone@gmail.com>
2022-03-15 10:56:47 -0300

    creata struttura vue

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        user: {
+                nome: 'Sofia',
+                avatar: 'io.jpg'
+            },
+        arrContacts: [
+            {
+                nome: 'Michele',
+                avatar: '1.jpg'
+            },
+            {
+                nome: 'Fabio',
+                avatar: '2.jpg'
+            },
+            {
+                nome: 'Samuele',
+                avatar: '3.jpg'
+            },
+            {
+                nome: 'Alessandro B.',
+                avatar: '4.jpg'
+            },
+            {
+                nome: 'Alessandro L.',
+                avatar: '5.jpg'
+            },
+            {
+                nome: 'Claudia',
+                avatar: '6.jpg'
+            },
+            {
+                nome: 'Federico',
+                avatar: '7.jpg'
+            },
+            {
+                nome: 'Davide',
+                avatar: '8.jpg'
+            }
+        ]
+    }
+
+})
